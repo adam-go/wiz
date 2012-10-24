9deab2a294b9eca88031922b5198c7c0c603e5cd
Gould <gouldar02@JMACADMIN002.local>
2012-10-24 09:50:13 -0400

    Adding files

---
+++ js/wiz.js
@@ -0,0 +1,26 @@
+var Wizard = function (selector) {
+    var n = this.jNode = $(selector);
+    this.slides = $('.slides', n);
+    this.currentPane = $('fieldset:first-child', this.slides);
+    $('.actions button.next', n).on('click', _.bind(this.nextSlide, this));
+    $('.actions button.prev', n).on('click', _.bind(this.prevSlide, this));
+}
+
+Wizard.prototype = {
+    nextSlide: function () {
+        this.moveSlide(this.currentPane.next(), function (ml, w) { return ml - w; });
+    },
+    
+    prevSlide: function () {
+        this.moveSlide(this.currentPane.prev(), function (ml, w) { return ml + w; });
+    },
+    
+    moveSlide: function (slide, cb) {
+        if(slide.length == 0) return;
+        var w = this.currentPane.outerWidth();
+        var ml = parseInt(this.slides.css('margin-left'));
+        this.slides.animate({marginLeft: cb(ml, w) + 'px'}, 400, 'swing');
+        this.currentPane = slide;
+    }
+};
+​
